5bb3856e686c394134438460b5b392c61ade47e5
Sai7xp <saisumanth2303@gmail.com>
2024-07-05 20:01:01 +0530

    Majority Element - Markdown test

---
+++ LeetCode/Arrays/MajorityElement_169/MajorityElement.java
@@ -0,0 +1,12 @@
+package MajorityElement_169;
+/*
+* Created on 05 July 2024
+* 
+* @author Sai Sumanth
+*/
+
+class MajorityElement {
+    public static void main(String[] args) {
+        System.out.println("LC 169 - Majority Element");
+    }
+}
